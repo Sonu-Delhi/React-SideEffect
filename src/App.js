c40ac93d4673bd7f7b70a7e99b3ba5311013263b
Sonu-Delhi <sy9812672@gmail.com>
2024-06-13 22:09:11 +0530

    React SideEffect in functional component

--- src/App.js
+++ src/App.js
@@ -1,25 +1,37 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useEffect, useState } from 'react'
+import './App.css'
+const App = () => {
+  const [count, setCount]=useState(0)
+  const increment=()=>{
+    setCount(count+1)
+  }
+  const decrement=()=>{
+    if(count>0){
+      setCount(count-1)
+    }else{
+      alert("Count can't be less than 0")
+    }
 
-function App() {
+  }
+  const reset=()=>{
+    setCount(0)
+  }
+  useEffect(()=>{
+    let interval = setInterval(()=>{
+      setCount(count+1)
+    },1000)
+    return ()=>{
+      clearInterval(interval)
+    }
+  })
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className='App'>
+      <p className='fs-2 fw-bold text-danger'>{count}</p>
+      <button className='btn btn-primary fw-bold' onClick={increment}>Increment</button>
+      <button className='btn btn-warning mx-2 fw-bold' onClick={decrement}>Decrement</button>
+      <button className='btn btn-danger mx-2 fw-bold' onClick={reset}>Reset</button>
     </div>
-  );
+  )
 }
 
-export default App;
+export default App
